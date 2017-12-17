e8076362c61253ea2167ac0b3540f5a91233c6cb
Simon Berger <siku2@outlook.de>
2017-12-17 23:20:02 +0100

    maybe?

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
 
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.6"
     ],
     keywords="lyrics",
