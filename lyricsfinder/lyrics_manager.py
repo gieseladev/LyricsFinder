7fd777c825a42bf46bff20c372866dac81a6665f
Simon Berger <siku2@outlook.com>
2018-07-31 17:17:58 +0200

    added CLI

--- lyricsfinder/lyrics_manager.py
+++ lyricsfinder/lyrics_manager.py
@@ -1,5 +1,6 @@
+import importlib
 import logging
-from typing import Iterator
+from typing import Iterator, List
 
 from .extractor import LyricsExtractor
 from .models import Lyrics, LyricsOrigin, exceptions
@@ -11,24 +12,26 @@ log = logging.getLogger(__name__)
 class LyricsManager:
     """Manage stuff."""
 
-    extractors = []
+    extractors: List[LyricsExtractor] = []
 
-    google_api_key = None
+    google_api_key: str = None
 
     @classmethod
     def setup(cls):
         """Load extractors."""
         log.debug("setting up")
-        # noinspection PyUnresolvedReferences
-        from . import extractors
+        importlib.import_module(".extractors", package=__package__)
         cls.extractors = LyricsExtractor.extractors
         log.info("loaded {} extractors".format(len(cls.extractors)))
 
     @classmethod
     def extract_lyrics(cls, url: str) -> Lyrics:
         """Extract lyrics from url."""
+        exc = None
+
         log.info("extracting lyrics from url \"{}\"".format(url))
         url_data = UrlData(url)
+
         for extractor in cls.extractors:
             if extractor.can_handle(url_data):
                 log.debug("using {} for {}".format(extractor, url_data))
@@ -40,18 +43,26 @@ class LyricsManager:
                 except exceptions.NotAllowedError:
                     log.warning("{} couldn't access lyrics at {}".format(extractor, url))
                     continue
-                except Exception:
+                except Exception as e:
+                    e.__cause__ = exc
+                    exc = e
                     log.exception("Something went wrong when {} handled {}".format(extractor, url_data))
                     continue
                 else:
                     lyrics.set_origin(LyricsOrigin(url, extractor.name, extractor.url))
                     log.debug("extracted lyrics {}".format(lyrics))
                     return lyrics
-        raise exceptions.NoExtractorError(url)
+        raise exceptions.NoExtractorError(url) from exc
 
     @classmethod
-    def search_lyrics(cls, query: str, *, google_api_key: str) -> Iterator[Lyrics]:
+    def search_lyrics(cls, query: str, *, google_api_key: str = None) -> Iterator[Lyrics]:
         """Search the net for lyrics."""
+        if not google_api_key:
+            if cls.google_api_key:
+                google_api_key = cls.google_api_key
+            else:
+                raise TypeError("search requires \"google_api_key\" to be present!")
+
         results = search(query, google_api_key)
         log.debug("got {} results".format(len(results)))
         for result in results:
